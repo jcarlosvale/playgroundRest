5369f15f43ceb2434d3f674e4a1ee05c7f21b591
Joao Carlos Sousa do Vale <joaocarlos.sousadovale@tipico.com>
2022-05-04 12:23:01 +0200

    adicionando exemplos PUT DELETE POST

--- src/main/java/com/playground/controller/HelloWorldController.java
+++ src/main/java/com/playground/controller/HelloWorldController.java
@@ -1,13 +1,22 @@
 package com.playground.controller;
 
 import com.playground.dto.HelloWorldBean;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
+@Log4j2
 public class HelloWorldController {
 
     @GetMapping(path = "/hello-world", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -25,13 +34,26 @@ public class HelloWorldController {
         return new ResponseEntity<>("Hello World 2 " + name, HttpStatus.OK);
     }
 
-    @GetMapping(path = "/hello-world-body", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> helloWorldBody(@RequestBody String name) {
-        return new ResponseEntity<>("Hello World 3 " + name, HttpStatus.OK);
-    }
-
     @GetMapping(path = "/exception", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> helloWorldException() {
         throw new RuntimeException("Hello Word Example");
+    }
+
+    @PostMapping(path = "/hello-world", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> helloWorld(@RequestBody HelloWorldBean helloWorldBean) {
+        log.info("POST " + helloWorldBean);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/hello-world/{name}")
+    public ResponseEntity<String> helloWorldDelete(@PathVariable String name) {
+        log.info("DELETE " + name);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(path = "/hello-world")
+    public ResponseEntity<Void> helloWorldPut(@RequestBody HelloWorldBean helloWorldBean) {
+        log.info("PUT " + helloWorldBean);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
--- src/main/java/com/playground/dto/HelloWorldBean.java
+++ src/main/java/com/playground/dto/HelloWorldBean.java
@@ -1,10 +1,13 @@
 package com.playground.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class HelloWorldBean {
-    private final String message;
+    private String message;
 }
